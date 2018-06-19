6b8c85671054d439cb12df326ae1f7d8d8c08e30
AJ Henley <ajhenley@gmail.com>
2018-06-19 11:54:50 -0400

    Monopoly Dice with Pictures and No Animation

--- MonopolyDice/ViewController.swift
+++ MonopolyDice/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var leftDie: UIImageView!
+    @IBOutlet weak var rightDie: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        var leftNumber = Int(arc4random_uniform(5)) + 1
+        var rightNumber = Int(arc4random_uniform(5)) + 1
+        var leftIm:String = ""
+        var rightIm:String = ""
+        
+        leftIm = "dice\(leftNumber).jpg"
+        rightIm = "dice\(rightNumber).jpg"
+
+        leftDie.image = UIImage(named: leftIm)
+        rightDie.image = UIImage(named: rightIm)
+        
+        if leftNumber == rightNumber {
+            displayLabel.textColor = UIColor.red
+        } else {
+            displayLabel.textColor = UIColor.black
+        }
+        
+        displayLabel.text = String(leftNumber + rightNumber)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
